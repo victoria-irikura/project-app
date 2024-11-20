064eb704ef1bb1317e0a1b431ffda618928c9050
Victoria Irikura <victoria.irikura@outlook.com>
2024-11-20 12:36:30 +0000

    initial commit

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ car_data = pd.read_csv('https://raw.githubusercontent.com/victoria-irikura/proje
 
 st.header('Análise do dados de anúncios de vendas de carros')
 
+st.write('Histograma de km rodados pelos veículos')
 # criar botão para geração do histograma
 hist_button = st.button('Criar histograma')
 
@@ -19,16 +20,16 @@ if hist_button: # se o botão for clicado
     # exibir um gráfico Plotly interativo
     st.plotly_chart(fig, use_container_width=True)
 
+st.write('Gráfico de dispersão tipo de veículo vs preço')
 # criar uma caixa de seleção
 build_scatter = st.checkbox('Criar gráfico de dispersão')
 
-
 if build_scatter: # se a caixa de seleção for selecionada
     # escrever uma mensagem
-    st.write('Criando um gráfico de dispersão para a coluna odometer')
+    st.write('Criando um gráfico de dispersão para a coluna tipo de veículo')
 
     # criar um gráfico de dispersão
-    fig = px.scatter(car_data, x="odometer", y="price")
+    fig = px.scatter(car_data, x="type", y="price")
 
     # exibir um gráfico Plotly interativo
     st.plotly_chart(fig, use_container_width=True)
